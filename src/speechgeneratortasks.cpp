f7b80fb1c8061b67d7a51a6af6fa3a23c937f4c6
Nonato <rnonato.lagunas@gmail.com>
2016-05-10 17:17:43 -0500

    bug corrected: does not validate ros initialization before create a new node handler and serviceclient

--- src/speechgeneratortasks.cpp
+++ src/speechgeneratortasks.cpp
@@ -8,35 +8,43 @@ SpeechGeneratorTasks::SpeechGeneratorTasks(std::string syncSpeechServName,
 
 bool SpeechGeneratorTasks::syncSpeech(std::string textToSpeech, int timeOut)
 {
-    ros::NodeHandle nodeHandler;
-    ros::ServiceClient client = nodeHandler.serviceClient
-        <bbros_bridge::Default_ROS_BB_Bridge>(m_syncSpeechServName);
-
-    bbros_bridge::Default_ROS_BB_Bridge srv;
-    srv.request.parameters = textToSpeech;
-    srv.request.timeout = timeOut;
-
-    if(client.call(srv))
+    if(ros::isInitialized())
     {
-        return true;
+        ros::NodeHandle nodeHandler;
+        ros::ServiceClient client = nodeHandler.serviceClient
+            <bbros_bridge::Default_ROS_BB_Bridge>(m_syncSpeechServName);
+
+        bbros_bridge::Default_ROS_BB_Bridge srv;
+        srv.request.parameters = textToSpeech;
+        srv.request.timeout = timeOut;
+
+        if(client.call(srv))
+        {
+            return true;
+        }
+        return false;
     }
+    
     return false;
 }
 
 void SpeechGeneratorTasks::asyncSpeech(std::string textToSpeech)
 {
-    ros::NodeHandle nodeHandler;
-    ros::ServiceClient client = nodeHandler.serviceClient
-        <bbros_bridge::Default_ROS_BB_Bridge>(m_asyncSpeechServName);
+    if(ros::isInitialized())
+    {
+        ros::NodeHandle nodeHandler;
+        ros::ServiceClient client = nodeHandler.serviceClient
+            <bbros_bridge::Default_ROS_BB_Bridge>(m_asyncSpeechServName);
 
-    bbros_bridge::Default_ROS_BB_Bridge srv;
-    srv.request.parameters = textToSpeech;
-    srv.request.timeout = 0;
+        bbros_bridge::Default_ROS_BB_Bridge srv;
+        srv.request.parameters = textToSpeech;
+        srv.request.timeout = 0;
 
-    client.call(srv);
-    //if(client.call(srv))
-    //{
-    //    return;
-    //}
-    //return ;
+        client.call(srv);
+        //if(client.call(srv))
+        //{
+        //    return;
+        //}
+        //return ;
+    }
 }
